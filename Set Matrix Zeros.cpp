0b6285e1c14d082cc4286f54f379b4262d978c2d
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2022-07-05 13:32:48 +0530

    Update Set Matrix Zeros.cpp

--- Set Matrix Zeros.cpp
+++ Set Matrix Zeros.cpp
@@ -1,3 +1,5 @@
+/* Problem link:- https://www.codingninjas.com/codestudio/problems/set-matrix-zeros_3846774?topList=striver-sde-sheet-problems */
+
 #include <bits/stdc++.h> 
 void setZeros(vector<vector<int>> &matrix)
 {

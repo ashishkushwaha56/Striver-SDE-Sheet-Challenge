c182eae49eb45ae081d7bf549bdeb1df82f9b458
Ashish Kushwaha <108444225+ashishkushwaha56@users.noreply.github.com>
2022-07-05 11:00:22 +0530

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    vector<int> ans;
+    int t = n-1;
+    if(n == 1){
+        return permutation;
+    }
+    bool ok = true;
+    for(int i = n-1;i>0;i--){
+        if(permutation[i-1]<permutation[i]){
+            ok = false;
+            t = i-1;
+            break;
+        }
+    }
+    if(permutation[n-1]>permutation[n-2]){
+        swap(permutation[n-1],permutation[n-2]);
+        return permutation;
+    }
+    if(ok){
+        sort(permutation.begin(),permutation.end());
+        return permutation;
+    }
+    int required = permutation[t];
+    vector<int> vd;
+    for(int i = t;i<n;i++){
+//         if(permutation[i] == required){
+//             continue;
+//         }
+        vd.push_back(permutation[i]);
+    }
+    sort(vd.begin(),vd.end());
+    auto it = upper_bound(vd.begin(),vd.end(),required);
+    int h = *it;
+    vd.erase(vd.begin()+(it-vd.begin()));
+    
+    for(int i = 0;i<t;i++){
+        ans.push_back(permutation[i]);
+    }
+    ans.push_back(h);
+    for(auto &x:vd){
+        ans.push_back(x);
+    }
+    return ans;
+    
+}
